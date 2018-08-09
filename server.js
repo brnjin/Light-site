088cb45fde7689ef360f68f57d2d0affe87bd4c1
brnjin <brnjin6gamil.com>
2018-08-09 19:26:20 -0400

    added js file

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const path = require('path');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3030;
 let app = express();
 
 app.use(express.static(__dirname));
 
 app.get('*', (req,res) => {
-	res.send(path.resolve(__dirname, '/index.html'))
+  res.send(path.resolve(__dirname, '/index.html'))
 });
 
 app.listen(PORT, function() {
-	console.log("App is listening on Port " + PORT);
+  console.log("App is listening on Port " + PORT);
 });
