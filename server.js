b32afb1c896dc1339609ec91d47eca94bd4565a2
brnjin <brnjin6gamil.com>
2018-09-21 00:59:13 -0400

    init

--- server.js
+++ server.js
@@ -3,10 +3,9 @@ const path = require('path');
 const PORT = process.env.PORT || 3030;
 let app = express();
 
-app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './index.html'))
+  res.sendFile(path.join(__dirname, './public/index.html'))
 });
 
 app.listen(PORT, function() {
