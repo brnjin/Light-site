4b66e792c444d33eea0a19c6b117a0a727cdd32e
brnjin <brnjin6gamil.com>
2018-09-27 01:56:32 -0400

    multi page react app

---
+++ src/pages/ItemList.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Cards from '../components/Cards';
+
+class ItemList extends Component {
+
+	render() {
+		return(
+			<div> 
+				<h3 className = "headers">CHANDELIERS</h3>
+	        <Cards />
+	        <h3 className = "headers">PENDANTS</h3>
+	        <Cards />
+	        <h3 className = "headers">SCONCES</h3>
+	        <Cards />
+	        <h3 className = "headers">TABLE LAMPS</h3>
+	        <Cards />
+	        <h3 className = "headers">FLOOR LAMPS</h3>
+	        <Cards /> 
+			</div> 
+		)
+	}
+}
+
+export default ItemList;
