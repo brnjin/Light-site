d4216abf12970bdd87a12659464b1b17f8d89592
brnjin <brnjin6gamil.com>
2018-10-10 06:19:41 -0800

    router

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Router, Route, IndexRoute } from 'react-router'
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const app = document.getElementById('app');
+
+ReactDOM.render(<App />, app);
 registerServiceWorker();
